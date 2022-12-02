1a5009b040ff19ca449a84981602bd7a1e5af761
emathian <mathian.emilie@gmail.com>
2022-12-02 10:30:06 +0100

    Remove validation

--- Bash/Train/TCAC_with_barlowtwin/TrainCflowBarlowTwin_AC0_set2.sh
+++ Bash/Train/TCAC_with_barlowtwin/TrainCflowBarlowTwin_AC0_set2.sh
@@ -16,4 +16,4 @@ module purge
 export PYTHONUSERBASE=/gpfswork/rech/uli/ueu39kt/.local_base_timm
 module load pytorch-gpu/py3/1.9.0
 mkdir /gpfsscratch/rech/uli/ueu39kt/CFLOW/weights/TrainCflowBarlowTwin_AC0_set2
-srun python /linkhome/rech/genkmw01/ueu39kt/cflow-ad/main_cflow.py --gpu 0 -inp 384 --dataset TCAC   --root-data-path /gpfsscratch/rech/uli/ueu39kt/Tiles_HE_all_samples_384_384_Vahadane_2 --weights-dir /gpfsscratch/rech/ohv/ueu39kt/CFLOW/weights/TrainCflowBarlowTwin_AC0_set2 --list-file-train train_tiles_cfow_barlowtwins_set2_all.txt --list-file-test validation_ac_5_list.txt --meta-epochs 25 --sub-epochs 8 --parallel --lr 2e-4 --enc-arch wide_resnet50_barlowtwin_LNEN
+srun python /linkhome/rech/genkmw01/ueu39kt/cflow-ad/main_cflow.py --gpu 0 -inp 384 --dataset TCAC  --root-data-path /gpfsscratch/rech/uli/ueu39kt/Tiles_HE_all_samples_384_384_Vahadane_2 --weights-dir /gpfsscratch/rech/ohv/ueu39kt/CFLOW/weights/TrainCflowBarlowTwin_AC0_set2 --list-file-train train_tiles_cfow_barlowtwins_set2_all.txt --list-file-test validation_ac_5_list.txt --meta-epochs 25 --sub-epochs 8 --parallel --lr 2e-4 --enc-arch wide_resnet50_barlowtwin_LNEN
--- train.py
+++ train.py
@@ -118,102 +118,7 @@ def train_meta_epoch(c, epoch, loader, encoder, decoders, optimizer, pool_layers
         mean_train_loss = train_loss / train_count
         if c.verbose:
             print('Epoch: {:d}.{:d} \t train loss: {:.4f}, lr={:.6f}'.format(epoch, sub_epoch, mean_train_loss, lr))
-    #
-
-
-def test_meta_epoch(c, epoch, loader, encoder, decoders, pool_layers, N):
-    # test
-    if c.verbose:
-        print('\nCompute loss and scores on test set:')
-    #
-    P = c.condition_vec
-    decoders = [decoder.eval() for decoder in decoders]
-    height = list()
-    width = list()
-    image_list = list()
-    gt_label_list = list()
-    gt_mask_list = list()
-    test_dist = [list() for layer in pool_layers]
-    test_loss = 0.0
-    test_count = 0
-    start = time.time()
-    files_path_list = []
-    loss_list = []
-    I = len(loader)
-
-    with torch.no_grad():
-        for i, (image, label, mask, filespath) in enumerate(tqdm(loader, disable=c.hide_tqdm_bar)):
-            if i % 10 == 0:
-                print('step  % : ', (i/I) * 100, ' i/I = ', i , '/' , I)
-            files_path_list.append(filespath)
-            # save
-            if c.viz:
-                image_list.extend(t2np(image))
-            gt_label_list.extend(t2np(label))
-            gt_mask_list.extend(t2np(mask))
-            # data
-            image = image.to(c.device) # single scale
-            _ = encoder(image)  # BxCxHxW
-            # test decoder
-            e_list = list()
-            for l, layer in enumerate(pool_layers):
-                if 'vit' in c.enc_arch:
-                    e = activation[layer].transpose(1, 2)[...,1:]
-                    e_hw = int(np.sqrt(e.size(2)))
-                    e = e.reshape(-1, e.size(1), e_hw, e_hw)  # BxCxHxW
-                else:
-                    e = activation[layer]  # BxCxHxW
-                #
-                B, C, H, W = e.size()
-                S = H*W
-                E = B*S
-                #
-                if i == 0:  # get stats
-                    height.append(H)
-                    width.append(W)
-                #
-                p = positionalencoding2d(P, H, W).to(c.device).unsqueeze(0).repeat(B, 1, 1, 1)
-                c_r = p.reshape(B, P, S).transpose(1, 2).reshape(E, P)  # BHWxP
-                e_r = e.reshape(B, C, S).transpose(1, 2).reshape(E, C)  # BHWxC
-                #
-                m = F.interpolate(mask, size=(H, W), mode='nearest')
-                m_r = m.reshape(B, 1, S).transpose(1, 2).reshape(E, 1)  # BHWx1
-                #
-                decoder = decoders[l]
-                FIB = E//N + int(E%N > 0)  # number of fiber batches
-                for f in range(FIB):
-                    if f < (FIB-1):
-                        idx = torch.arange(f*N, (f+1)*N)
-                    else:
-                        idx = torch.arange(f*N, E)
-                    #
-                    c_p = c_r[idx]  # NxP
-                    e_p = e_r[idx]  # NxC
-                    m_p = m_r[idx] > 0.5  # Nx1
-                    #
-                    if 'cflow' in c.dec_arch:
-                        z, log_jac_det = decoder(e_p, [c_p,])
-                    else:
-                        z, log_jac_det = decoder(e_p)
-                    #
-                    decoder_log_prob = get_logp(C, z, log_jac_det)
-                    log_prob = decoder_log_prob / C  # likelihood per dim
-                    loss = -log_theta(log_prob)
-                    test_loss += t2np(loss.sum())
-                    test_count += len(loss)
-                    test_dist[l] = test_dist[l] + log_prob.detach().cpu().tolist()
-                    #
-            
-            if i % 1000 == 0 :
-                print('Epoch: {:d} \t step: {:.4f} '.format(epoch, i))
-    fps = len(loader.dataset) / (time.time() - start)
-    mean_test_loss = test_loss / test_count
-    if c.verbose:
-        print('Epoch: {:d} \t test_loss: {:.4f} and {:.2f} fps'.format(epoch, mean_test_loss, fps))
-    #
-    return height, width, image_list, test_dist, gt_label_list, gt_mask_list, files_path_list
-
-
+    
 def test_meta_epoch_lnen(c, epoch, loader, encoder, decoders, pool_layers, N):
     # test
     print('\nCompute loss and scores on test set:')
@@ -447,10 +352,6 @@ def train(c):
             test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=c.batch_size, shuffle=True, drop_last=False, **kwargs)
     N = 256  # hyperparameter that increases batch size for the decoder model by N
   
-    # stats
-    det_roc_obs = Score_Observer('DET_AUROC')
-    seg_roc_obs = Score_Observer('SEG_AUROC')
-    seg_pro_obs = Score_Observer('SEG_AUPRO')
     if c.action_type == 'norm-test':
         c.meta_epochs = 1
     for epoch in range(c.meta_epochs):  
@@ -473,38 +374,18 @@ def train(c):
         else:
             raise NotImplementedError('{} is not supported action type!'.format(c.action_type))
         
-        #height, width, test_image_list, test_dist, gt_label_list, gt_mask_list = test_meta_fps(
-        #    c, epoch, test_loader, encoder, decoders, pool_layers, N)
-        # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        #if not c.parallel:    
-        
-        if c.dataset != 'TumorNormal' and c.dataset != 'TCAC' :
-            print('Len test_loader  ', len(test_loader))
+    
+        if c.action_type != 'norm-train':
+            # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            # LINE FOR EVALUATION
+            # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             if c.parallel:
-                print('Eval model parallel')
-                height, width, test_image_list, test_dist, gt_label_list, gt_mask_list, files_path_list = test_meta_epoch(
-            c, epoch, test_loader, ddp_encoder, ddp_decoders, pool_layers, N)
+                print('EVAL IN C.PARALLEL test_meta_epoch_lnen')
+                height, width, test_image_list, test_dist, gt_label_list, gt_mask_list, files_path_list = test_meta_epoch_lnen(
+            c, epoch, test_loader, ddp_encoder, ddp_decoders, pool_layers, N) 
             else:
-                height, width, test_image_list, test_dist, gt_label_list, gt_mask_list, files_path_list = test_meta_epoch(
-            c, epoch, test_loader, encoder, decoders, pool_layers, N)
-        else:
-            if c.action_type == 'norm-train':
-                #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                #LINE FOR VALIDATION
-                #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                height, width, test_image_list, test_dist, gt_label_list, gt_mask_list, files_path_list = test_meta_epoch(
-                c, epoch, test_loader, encoder, decoders, pool_layers, N)
-            else:
-                # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                # LINE FOR EVALUATION
-                # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                if c.parallel:
-                    print('EVAL IN C.PARALLEL test_meta_epoch_lnen')
-                    height, width, test_image_list, test_dist, gt_label_list, gt_mask_list, files_path_list = test_meta_epoch_lnen(
-                c, epoch, test_loader, ddp_encoder, ddp_decoders, pool_layers, N) 
-                else:
-                    print('TEst not paraller LNEN')
-                    height, width, test_image_list, test_dist, gt_label_list, gt_mask_list, files_path_list = test_meta_epoch_lnen(
-                c, epoch, test_loader, encoder, decoders, pool_layers, N) # test_meta_epoch_lnen
-        
+                print('Test not paraller LNEN')
+                height, width, test_image_list, test_dist, gt_label_list, gt_mask_list, files_path_list = test_meta_epoch_lnen(
+            c, epoch, test_loader, encoder, decoders, pool_layers, N) # test_meta_epoch_lnen
+
 
